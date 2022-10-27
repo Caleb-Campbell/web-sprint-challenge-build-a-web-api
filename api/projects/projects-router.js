37aca355db82def1bfdd5cb46ca2ade80acb50f0
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-10-27 10:57:46 -0600

    get, getID, and post are all working on P router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,105 @@
 // Write your "projects" router here!
+const express = require('express')
+const router = express.Router()
+
+// model functions imported for use
+const Projects = require('./projects-model')
+
+router.get('/', (req, res) => {
+    Projects.get()
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(error => {
+        res.status(500).json([])
+    })
+})
+
+router.get('/:id', (req, res)=> {
+    const {id} = req.params
+   Projects.get(id)
+   .then(data => {
+    if(!data){
+        res.status(404).json({
+            message: 'project with that id not found'
+        })
+    }
+    else {
+        res.status(200).json(data)
+    }
+   })
+   .catch(error => {
+    console.log(error)
+    res.status(500)
+})
+
+})
+
+router.post('/', (req, res) => {
+    const {id, name, description, completed} = req.body
+    if(!name || !description || !completed){
+        res.status(400).json({
+            message: 'Cannot have missing fields'
+        })
+    }
+    else {
+        Projects.insert(req.body)
+        .then(data => {
+            res.status(201).json(data)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500)
+        })
+        
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const {id} = req.params
+    const {name, description, completed} = req.body
+    if(!Projects.get(id)){
+        res.status(204).json({
+            message: 'No project with that id could be found'
+        })
+    }
+    else if (!req.body) {
+        res.status(400).json({
+            message: 'Please include all fields'
+        })
+    }
+    else {
+        Projects.update(id, req.body)
+        .then(data => {
+            res.status(201).json(data)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500)
+        })
+
+    }
+    
+
+})
+
+
+
+
+
+
+
+module.exports = router
+
+// .catch(error => {
+//     console.log(error)
+//     res.status(500)
+// })
+
+// module.exports = {
+//     get,
+//     insert,
+//     update,
+//     remove,
+//     getProjectActions,
+//   };
--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+// imported routers
+const projectRouter = require('./projects/projects-router')
+const actionRouter = require('./actions/actions-router')
+
+server.use(express.json())
+
+// routes set as middleware
+server.use('/api/projects', projectRouter)
+server.use('/api/actions', actionRouter)
+
+// default message
+server.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the Express Server'
+    })
+})
 
 module.exports = server;
